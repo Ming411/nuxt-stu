c5d18330454e7d7d491b1d13855db78147ff1564
cxm <18755634050@163.com>
2022-09-07 17:58:32 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  router: {
+    extendRoutes(routes, resolve) {
+      // 清除Nuxt默认生成的路由表
+      routes.splice(0);
+      routes.push(
+        ...[
+          {
+            path: '/',
+            // 默认查找index.vue
+            component: resolve(__dirname, 'pages/layout/'),
+            children: [
+              {
+                path: '', // 默认子路由
+                name: 'home',
+                component: resolve(__dirname, 'pages/home/')
+              },
+              {
+                path: '/login',
+                name: 'login',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                path: '/register',
+                name: 'register',
+                component: resolve(__dirname, 'pages/login/')
+              }
+            ]
+          }
+        ]
+      );
+    }
+  }
+};
